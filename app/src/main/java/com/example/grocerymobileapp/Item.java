8ab7d86e68249b57d4927d323d72c343eaf5e91e
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-09-29 16:34:49 +0800

    feat: create basic Item class

---
+++ app/src/main/java/com/example/grocerymobileapp/Item.java
@@ -0,0 +1,19 @@
+package com.example.grocerymobileapp;
+
+public class Item {
+    private String name;
+    private double price;
+
+    public Item (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+}
